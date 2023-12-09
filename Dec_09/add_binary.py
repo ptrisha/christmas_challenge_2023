063c113698f233802b4641610d017e8d6d81f07e
ptrisha <104321566+ptrisha@users.noreply.github.com>
2023-12-10 00:09:37 +0800

    Add comments in leetcode problem file

--- Dec_09/add_binary.py
+++ Dec_09/add_binary.py
@@ -1,5 +1,9 @@
+# Day 9 of Christmas Challenge
+# Problem statement:
+# https://leetcode.com/problems/add-binary
+
 class Solution:
-    
+
     def addBinary(self, a: str, b: str) -> str:
 
         int_a = int(a, 2)
@@ -7,4 +11,4 @@ class Solution:
         sum_str = bin(int_a + int_b)[2:]
 
         return sum_str
-    
+
