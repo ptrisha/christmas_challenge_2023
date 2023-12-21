9a6436c7b66e942b1779a1811b8966d2ac1e3be6
ptrisha <104321566+ptrisha@users.noreply.github.com>
2023-12-22 04:50:24 +0800

    Add second version of code for leetcode problem of Dec 21

--- Dec_21/word_break.py
+++ Dec_21/word_break.py
@@ -2,51 +2,37 @@
 # Problem description:
 # https://leetcode.com/problems/word-break
 
-# A preliminary version that does not work.
+# A second version that passes the submission test cases,
+# but still a bit slow.
 
 class Solution:
     def wordBreak(self, s: str, wordDict: List[str]) -> bool:
 
         length = len(s)
         memo = [None]*(length+1)
-        memo[0] = True
+        #memo[0] = True
 
-        word_lengths = [len(s) for s in wordDict]
-        max_wordlen = max(word_lengths)
-        min_wordlen = min(word_lengths)
-        print(f"Min word len: {min_wordlen}")
-        print(f"Max word len: {max_wordlen}")
 
-        for i in range(1, min_wordlen):
-            memo[i] = False
+        def creatable(sub: str, start_indx: int) -> bool:
+            print(f"str: {sub} start_indx: {start_indx}")
+ 
+            if memo[start_indx] is not None:
+                return memo[start_indx]
 
-        def creatable(sub: str) -> bool:
-            print(f"str: {sub} ")
-            s_len = len(sub)
-            if memo[s_len] is not None:
-                return memo[s_len]
+            if len(sub)==0:
+                return True
         
-            if s_len == 0:
-                return True
 
-            if s_len < min_wordlen:
-               return False
-
-            if sub in wordDict:
-                return True
-
-            for i in range(min_wordlen, max_wordlen+1):
-                print(f"pre: {sub[:i]} post: {sub[i:]} i: {i}")
-                if creatable(sub[:i]):
-                    if creatable(sub[i:]):
-                        memo[s_len] = True
-                        return True
-
-            memo[s_len] = False
+            for w in wordDict:
+                suffix_indx = start_indx + len(w)
+                if sub.startswith(w) and creatable(sub[len(w):], suffix_indx):
+                    memo[start_indx] = True
+                    return True
+            
+            memo[start_indx]=False
             return False
 
-        ans = creatable(s)
 
-        return ans
-    
-    
+        ans = creatable(s, 0)
+
+        return memo[0]
