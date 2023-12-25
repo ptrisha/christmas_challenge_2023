767b306ccfd4c6c4ec3ce4eda30b1328c46c325e
ptrisha <104321566+ptrisha@users.noreply.github.com>
2023-12-26 02:34:29 +0800

    Amend leetcode problem of Dec 11 using sliding window algo

--- Dec_11/minimum_size_subarray_sum.py
+++ Dec_11/minimum_size_subarray_sum.py
@@ -2,20 +2,35 @@
 # Problem description:
 # https://leetcode.com/problems/minimum-size-subarray-sum/
 
-# This version exceeds the time limit.
-# Will use the sliding window algorithm in the next version.
+# This version uses the sliding window algorithm.  It passes all the
+# submission test cases on Leetcode, and does not exceed the time limit.
 
 class Solution:
-
     def minSubArrayLen(self, target: int, nums: List[int]) -> int:
-        if max(nums) >= target:
-            return 1
-        # From here, we deal with cases where the numbers are < target
-        # iterate through length of subsequence
-        # inner loop through moving window of subsequence length
-        for i in range(1, len(nums)+1):
-            for j in range( len(nums) - i + 1):
-                if sum( nums[j:j+i] ) >= target:
-                    return i
+    
+        head = tail = currSum = 0
+        length = len(nums)
+        minLength = length+1
+
+
+        while head < length:
+            currSum += nums[head]
+
+            if currSum >= target:
+                # try to move up the tail
+                minLength = min(minLength, head-tail+1)
+
+                while (currSum-nums[tail] >= target) and (tail < head):
+                       currSum -= nums[tail]
+                       if (head-tail) < minLength:
+                           minLength = head-tail
+                       tail += 1
+            head += 1
+            
+            
+        if minLength > length:
+            minLength = 0
         
-        return 0
+        return minLength
+
+
